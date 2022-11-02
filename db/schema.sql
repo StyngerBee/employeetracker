27f3f12319cc461a3161f9cb0394e73d38597f77
Curran Duke <cdsonics@yahoo.com>
2022-11-02 16:55:08 -0400

    added all seeds

--- db/schema.sql
+++ db/schema.sql
@@ -34,6 +34,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
     FOREIGN KEY (manager_id) REFERENCES manager(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO department (name)
+INSERT INTO department (names)
 VALUES  ('Middle Earth'),
         ('Hogwarts'),
         ('Westeros'),
@@ -7,13 +7,25 @@ VALUES  ('Middle Earth'),
 INSERT INTO roles (title, salary, department_id)
 VALUES ('Hobbit', 10.00, 1),
        ('Wizard', 0, 2),
-       ('White Walker', 1.00, 3),
-       ('The Avatar', 100.00, 4),
+       ("Brother of the Night's Watch", 1.00, 3),
        ('Fire Nation', 0.00, 4),
        ('Water Nation', 0.00, 4),
        ('Earth Nation', 0.00, 4);
 
 INSERT INTO manager (first_name, last_name, department_id, salary)
-VALUES ('Aang', '')
+VALUES ('Avatar', 'Aang', 4, 0.00),
+       ('Gandalf', 'The Grey', 1, 15.00),
+       ('Albus', 'Dumbledore', 2, 100.00),
+       ('Bran', 'Stark', 3, 200.00);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Toph', ' ', 6, 1),
+       ('Sokka', ' ', 5, 1),
+       ('Katara', ' ', 5, 1),
+       ('Zuko', ' ', 4, 1),
+       ('Frodo', 'Baggins', 1, 2),
+       ('Jon', 'Snow', 3, 4),
+       ('Harry', 'Potter', 2, 3);
+
 
     
