558e46361cf1198c1d327974b6d5f60cec50b0e3
Curran Duke <cdsonics@yahoo.com>
2022-11-03 15:07:22 -0400

    added connection to universe_db

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+
+// connects to universe_db database //
+const db = mysql.createConnection(
+    {
+        user: 'root',
+        password: 'Rowling#15',
+        database: 'universe_db'
+    },
+    console.log(`Connected to the universe_db.`)
+);
